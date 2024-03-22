4b4ead442d0bc97105326bf091ca55968e343d8b
favydabs <favourdabit@gmail.com>
2024-03-22 22:30:33 +0100

    about me

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -10,15 +10,16 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
-          </p>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
+            I'm a frontend developer with a solid foundation in software
+            engineering. Graduating from ALX Software Engineering has equipped
+            me with a strong skill set and a deep understanding of frontend
+            development principles. My journey doesn't stop there. I'm also a
+            proud graduate of Web3Bridge, further specializing in web2 advance.
+            With a keen eye for design and a knack for crafting engaging user
+            experiences.
+            Whether it's building responsive websites, optimizing user
+            interfaces. I'm always eager to tackle new challenges and push the
+            boundaries of what's possible in frontend development.
           </p>
         </div>
       </div>
--- src/components/Skills/port.js
+++ src/components/Skills/port.js
@@ -1,29 +1,30 @@
-//   
-  //   {
-  //     "id": "5",
-  //     "src": "./img/placeholder-image-2.png",
-  //     "title": "Bank-landing-page",
-  //     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
-  //     "demo": "View In Github"
-  //   },
-  //   {
-  //     "id": "7",
-  //     "src": "./img/placeholder-image-2.png",
-  //     "title": "to-do-list-web2acx",
-  //     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
-  //     "demo": "View In Github"
-  //   },
-  //   {
-  //     "id": "8",
-  //     "src": "./img/placeholder-image-2.png",
-  //     "title": "-chatGpt-clone ",
-  //     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
-  //     "demo": "View In Github"
-  //   },
-  //   {
-  //     "id": "9",
-  //     "src": "./img/placeholder-image-2.png",
-  //     "title": "linkedin_clone_web2acx ",
-  //     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
-  //     "demo": "View In Github"
-  //   }
+//
+//    {
+//     "id": "5",
+//     "src": "./img/placeholder-image-2.png",
+//     "title": "Bank-landing-page",
+//     "description":"My bank landing page app is meticulously crafted to provide users with a seamless and secure banking experience. With a clean and intuitive interface, users can effortlessly access account information, perform transactions, and explore banking services with ease.",
+//     "demo": " https://favydabs.github.io/Bank-landing-page/"
+//   }
+//   {
+//     "id": "6",
+//     "src": "./img/placeholder-image-2.png",
+//     "title": "to-do-list-web2acx",
+//     "description": "To-Do List App is here to transform the way you manage your daily activities.
+// With our user-friendly interface and intuitive design, our app makes it easy to create, prioritize, and track your to-do items with just a few taps. Whether you're juggling work projects, household chores, or personal goals, our app helps you stay focused and productive throughout the day.",
+//     "demo": "https://favydabs.github.io/to-do-list-web2acx/"
+//   },
+//   {
+//     "id": "7",
+//     "src": "./img/placeholder-image-2.png",
+//     "title": "-chatGpt-clone ",
+//     "description": "This project is a simple implementation of a ChatGPT clone using HTML, CSS, and JavaScript. It provides a basic chat interface where users can interact with a bot that generates responses using predefined rules.",
+//     "demo": "https://chat-gpt-clone-iota-brown.vercel.app/"
+//   },
+//   {
+//     "id": "8",
+//     "src": "./img/placeholder-image-2.png",
+//     "title": "linkedin_clone_web2acx ",
+//     "description": "This project is a clone of the popular professional networking platform, LinkedIn. It aims to replicate key features and functionalities of LinkedIn to provide users with a similar experience.",
+//     "demo": "https://linkedin-clone-web2acx-eight.vercel.app/"
+//   }
